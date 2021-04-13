01ddae5f26bf3aaea564a25ecebcee5c923d9c71
unknown <martin.kavala@gmail.com>
2021-04-13 16:06:56 +0200

    4. Přidání tlačítek s akcemi

--- js/main.js
+++ js/main.js
@@ -1,8 +1,25 @@
-$(function(){
-$(".deleni").on("click", function () {
-    $(this).nextAll().toggle();
-});
-$("h4").on("click", function () {
-    $(this).nextAll().toggle();
-});
+$(function () {
+    $(".deleni").on("click", function () {
+        $(this).nextAll().slideToggle();
+    });
+    $("h4").on("click", function () {
+        $(this).nextAll().toggle();
+    });
+        $("#varovani").hide();
+    $("#simulace").on("click", function(){
+        $("#varovani").show();
+    });
+    $("#blue").on("click", function(){
+        $("h2").css("background-color", "rgb(36, 87, 255)");
+        $("h5").css("color", "rgb(36, 87, 255)");
+        $(".pozadi").css("background-color", "rgb(36, 87, 255)");
+        $("span").css("background-color", "rgb(36, 87, 255)");
+        $("button").css("background-color", "rgb(36, 87, 255)");
+        $("#vlajka").attr("src","img/vlajka2.png");
+        $("#statni_znak").attr("src","img/statni_znak2.png");
+        $(".logo").attr("src","img/vlajka2.png");
+        $("#red").css("color", "rgb(36, 87, 255)").text("modrý kříž");  
+        $("#varovani").removeClass("alert-danger");
+        $("#varovani").addClass("alert-primary");
+    });
 });
