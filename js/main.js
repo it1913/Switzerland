41e679e59f24d290b422cc43dbc59a26d74b484b
unknown <martin.kavala@gmail.com>
2021-04-13 17:00:43 +0200

    5. Úprava tlačítek a validování stránky

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ $(function () {
     });
         $("#varovani").hide();
     $("#simulace").on("click", function(){
-        $("#varovani").show();
+        $("#varovani").toggle();
     });
     $("#blue").on("click", function(){
         $("h2").css("background-color", "rgb(36, 87, 255)");
@@ -18,8 +18,21 @@ $(function () {
         $("#vlajka").attr("src","img/vlajka2.png");
         $("#statni_znak").attr("src","img/statni_znak2.png");
         $(".logo").attr("src","img/vlajka2.png");
-        $("#red").css("color", "rgb(36, 87, 255)").text("modrý kříž");  
+        $("#cervena").css("color", "rgb(36, 87, 255)").text("modrý kříž");  
         $("#varovani").removeClass("alert-danger");
         $("#varovani").addClass("alert-primary");
+    });
+    $("#red").on("click", function(){
+        $("h2").css("background-color", "rgb(219, 0, 0)");
+        $("h5").css("color", "rgb(219, 0, 0)");
+        $(".pozadi").css("background-color", "rgb(219, 0, 0)");
+        $("span").css("background-color", "rgb(219, 0, 0)");
+        $("button").css("background-color", "rgb(219, 0, 0)");
+        $("#vlajka").attr("src","img/vlajka.png");
+        $("#statni_znak").attr("src","img/statni_znak.png");
+        $(".logo").attr("src","img/vlajka.png");
+        $("#cervena").css("color", "rgb(219, 0, 0)").text("červený kříž");  
+        $("#varovani").removeClass("alert-primary");
+        $("#varovani").addClass("alert-danger");
     });
 });
